e248b01319ee36964c6e2c50283a693705c86202
Brittany Morris <brittmmendez@gmail.com>
2018-03-29 01:42:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much.";
+const token = 'YOUR_TOKEN_HERE'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token bdb70fd22ad9c062429ec8b4b2d1ae140972ef2d`
+  }
+}).then(res => res.json()).then(json => console.log(json));
