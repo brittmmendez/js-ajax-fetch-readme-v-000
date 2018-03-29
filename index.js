be9f175bfdb51cf789aba5f55e08f4777f759b26
Brittany Morris <brittmmendez@gmail.com>
2018-03-29 14:38:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const token = 'YOUR_TOKEN_HERE'
 fetch('https://api.github.com/user/repos', {
   headers: {
+<<<<<<< HEAD
     Authorization: `token ${token}`
+=======
+    Authorization: `token 47fa3a304f088f7a690bf5582efacd5ec73f207a`
+>>>>>>> fa905821256cf3f4ba298fa7eb1c38ec1a177d16
   }
 }).then(res => res.json()).then(json => console.log(json));
