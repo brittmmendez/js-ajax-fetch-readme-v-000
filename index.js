3ff555f0d00acfc8f30d3e6a145683153a98701d
brittmmendez <33286529+brittmmendez@users.noreply.github.com>
2018-03-28 23:37:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const token = 'YOUR_TOKEN_HERE'
 fetch('https://api.github.com/user/repos', {
   headers: {
-    Authorization: `token bdb70fd22ad9c062429ec8b4b2d1ae140972ef2d`
+    Authorization: `token ${token}`
   }
 }).then(res => res.json()).then(json => console.log(json));
